7a29850a3b208b60b9459f6a1622febab1e8a472
Sasa Nedic <svetasmak0@gmail.com>
2024-03-18 19:57:05 +0100

    Add shuffle function with useEffect

--- Memory-cards/src/App.jsx
+++ Memory-cards/src/App.jsx
@@ -1,11 +1,24 @@
 import cards from './Data'
 import './App.css'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Cards from './Cards'
 
 function App() {
 const [data, setData] = useState(cards)
 
+useEffect(() => {
+
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  setData(shuffleArray(data));
+}, []);
+{console.log(data)}
   return (
     <>
     <h1>Rick and Morty memory game</h1>
