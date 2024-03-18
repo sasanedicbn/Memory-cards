b1962f23b9a1f45fa5590840796038715fa3c80d
Sasa Nedic <svetasmak0@gmail.com>
2024-03-18 20:05:05 +0100

    Clean code

--- Memory-cards/src/App.jsx
+++ Memory-cards/src/App.jsx
@@ -1,32 +1,36 @@
-import cards from './Data'
-import './App.css'
-import { useState,useEffect } from 'react'
-import Cards from './Cards'
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Cards from './Cards';
+import cards from './Data';
 
 function App() {
-const [data, setData] = useState(cards)
+  const [data, setData] = useState(cards);
 
-useEffect(() => {
+  const shuffleCards = () => {
+    const shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    };
 
-  const shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+    setData(shuffleArray([...data]));
   };
 
-  setData(shuffleArray(data));
-}, []);
-{console.log(data)}
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <>
-    <h1>Rick and Morty memory game</h1>
-    <h4>Current score: 0</h4>
-    <h4>High score: 0</h4>
-    <Cards data={data}/>
+      <h1>Rick and Morty memory game</h1>
+      <h4>Current score: 0</h4>
+      <h4>High score: 0</h4>
+      <Cards data={data} shuffleCards={shuffleCards} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Memory-cards/src/Cards.jsx
+++ Memory-cards/src/Cards.jsx
@@ -1,14 +1,18 @@
+
 import React from 'react';
 import './App.css';
 
-const Cards = ({ data }) => {
+const Cards = ({ data, shuffleCards }) => {
+  const handleCardClick = () => {
+    shuffleCards();
+  };
+
   return (
     <div className="container">
-      {data.map(card => (
-        <div className="card" key={card.id}>
-         <div className='name'>{card.name}</div>
-        <img src={card.img} alt={card.name} />
-         
+      {data.map((card, index) => (
+        <div className="card" key={index} onClick={handleCardClick}>
+          <div className='name'>{card.name}</div>
+          <img src={card.img} alt={card.name} />
         </div>
       ))}
     </div>
@@ -16,3 +20,4 @@ const Cards = ({ data }) => {
 };
 
 export default Cards;
+
