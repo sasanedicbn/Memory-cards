e6a9148349a2543be186646c83f584e381de0961
Sasa Nedic <svetasmak0@gmail.com>
2024-03-19 12:05:29 +0100

    Create Modal component

---
+++ Memory-cards/src/Modal.jsx
@@ -0,0 +1,11 @@
+const Modal = () => {
+    return(
+        <>
+        <h2>Game Over!</h2>
+        <p>Your score: </p>
+        <button>Play Again?</button>
+        </>
+    )
+}
+
+export default Modal;
