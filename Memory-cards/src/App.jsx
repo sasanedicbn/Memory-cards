fffe979eefce59af7de11b2d1033d5203ee35718
Sasa Nedic <svetasmak0@gmail.com>
2024-03-19 17:20:09 +0100

    Create hight score

--- Memory-cards/src/App.jsx
+++ Memory-cards/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Cards from './Cards';
@@ -7,29 +6,32 @@ import Modal from './Modal';
 
 function App() {
   const [data, setData] = useState(cards);
-  const [showModal, setShowModal] = useState(false)
-  const [clickedCard, setClickedCards] = useState([])
+  const [showModal, setShowModal] = useState(false);
+  const [clickedCard, setClickedCards] = useState([]);
+  const [highScore, setHighScore] = useState(0); 
 
   const handleCardClick = (card) => {
-    if(clickedCard.includes(card)){
-        openModal()
-        // setClickedCards([])
-    }else{
-        setClickedCards([...clickedCard, card]);
-        shuffleCards();
-        {console.log(clickedCard.length + 1)}
+    if (clickedCard.includes(card)) {
+      openModal();
+      if (clickedCard.length > highScore) { 
+        setHighScore(clickedCard.length); 
+      }
+    } else {
+      setClickedCards([...clickedCard, card]);
+      shuffleCards();
     }
   };
 
   function openModal() {
-      setShowModal(true)
+    setShowModal(true);
   }
-  function closeModal(){
-      setShowModal(false)
+
+  function closeModal() {
+    setShowModal(false);
   }
-  
+
   const shuffleCards = () => {
-    const shuffleArray = array => {
+    const shuffleArray = (array) => {
       for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -48,9 +50,14 @@ function App() {
     <>
       <h1>Rick and Morty memory game</h1>
       <h4>Current score: {clickedCard.length}</h4>
-      <h4>High score: 0</h4>
-      <Cards data={data} shuffleCards={shuffleCards} openModal={openModal} handleCardClick={handleCardClick}/>
-      {showModal && <Modal  closeModal={closeModal} clickedCard={clickedCard} setClickedCards={setClickedCards}/>}
+      <h4>High score: {highScore}</h4>
+      <Cards
+        data={data}
+        shuffleCards={shuffleCards}
+        openModal={openModal}
+        handleCardClick={handleCardClick}
+      />
+      {showModal && <Modal closeModal={closeModal} clickedCard={clickedCard} setClickedCards={setClickedCards} />}
     </>
   );
 }
